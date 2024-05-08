01fa14701893929283c8e5bf6b9ec0bfdc89d826
VoldemortSan <annaviworkss@gmail.com>
2024-05-08 15:28:45 +0530

    Update Areceived.py

--- Areceived.py
+++ Areceived.py
@@ -1,73 +1,94 @@
-def aStarAlgo(start_node, stop_node):
-    open_set = set(start_node)
-    closed_set = set()
-    g = {}               #store distance from starting node
-    parents = {}         # parents contains an adjacency map of all nodes
-    g[start_node] = 0 #distance of starting node from itself is zero
-    parents[start_node] = start_node #start_node is root node i.e it has no parent nodes so start_node is set to its own parent node
-    while len(open_set) > 0:
-        n = None #node with lowest f() is found
-        for v in open_set:
-            if n == None or g[v] + heuristic(v) < g[n] + heuristic(n):
-                n = v
-        if n == stop_node or Graph_nodes[n] == None:
-            pass
-        else:
-            for (m, weight) in get_neighbors(n):    #nodes 'm' not in first and last set are added to first,n is set its parent
-                if m not in open_set and m not in closed_set:
-                    open_set.add(m)
-                    parents[m] = n
-                    g[m] = g[n] + weight  #for each node m,compare its distance from start i.e g(m) to the ,from start through n node
-                else:
-                    if g[m] > g[n] + weight:   #update g(m)
-                        g[m] = g[n] + weight   #change parent of m to n
-                        parents[m] = n         #if m in closed set,remove and add to open
-                        if m in closed_set:
-                            closed_set.remove(m)
-                            open_set.add(m)
-        if n == None:
-            print('Path does not exist!')
-            return None 
-        # if the current node is the stop_node
-        # then we begin reconstructin the path from it to the start_node
-        if n == stop_node:
-            path = []
-            while parents[n] != n:
-                path.append(n)
-                n = parents[n]
-            path.append(start_node)
-            path.reverse()
-            print('Path found: {}'.format(path))
-            return path
-        # remove n from the open_list, and add it to closed_list
-        # because all of his neighbors were inspected
-        open_set.remove(n)
-        closed_set.add(n)
-    print('Path does not exist!')
-    return None
+from typing import List
+from copy import deepcopy
 
-#define fuction to return neighbor and its distance
-#from the passed node
-def get_neighbors(v):
-    if v in Graph_nodes:
-        return Graph_nodes[v]
-    else:
-        return None
+class Node:
+    def __init__(self,matrix : List[List[int]], g_x : int,goal: List[List[int]]) -> None:
+        self.matrix = matrix
+        self.g_x = g_x
+        self.h_x = 0
+        self.f_x = 0
+        self.goal = goal
+        self.n = 3
+        self.calculate_h_x()
+        self.calculate_f_x()
 
-def heuristic(n):
-    H_dist = {'A': 11,'B': 6,'C': 5,'D': 7,'E': 3,'F': 6,'G': 5,'H': 3,'I': 1,'J': 0}
-    return H_dist[n]
+    def locateZero(self):
+        for i,row in enumerate(self.matrix):
+            for j,ele in enumerate(row):
+                if ele == 0:
+                    return (i,j)
+    
+    def generateChildren(self):
+        x,y = self.locateZero()
+        possible = [
+            (x+1,y),
+            (x,y+1),
+            (x-1,y),
+            (x,y-1)
+        ]
+        children : List[Node] = []
+        for i,j in possible:
+            if i >= 0 and i < self.n and j >= 0 and j < self.n:
+                child = deepcopy(self.matrix)
+                child[x][y], child[i][j] = child[i][j], child[x][y]
+                children.append(Node(child,self.g_x + 1,self.goal))
+        return children
+    
+    def calculate_h_x(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.goal[i][j] != self.matrix[i][j]:
+                    self.h_x += 1
+        
+    def calculate_f_x(self):
+        self.f_x = self.g_x + self.h_x
+    
+    def printNode(self):
+        print(f"g(x) = {self.g_x}")
+        print(f"h(x) = {self.h_x}")
+        print(f"f(x) = {self.f_x}")
+        for i,row in enumerate(self.matrix):
+            for j,ele in enumerate(row):
+                print(ele,end=" ")
+            print()
+        print()
 
-Graph_nodes = {
-    'A': [('B', 6), ('F', 3)],
-    'B': [('A', 6), ('C', 3), ('D', 2)],
-    'C': [('B', 3), ('D', 1), ('E', 5)],
-    'D': [('B', 2), ('C', 1), ('E', 8)],
-    'E': [('C', 5), ('D', 8), ('I', 5), ('J', 5)],
-    'F': [('A', 3), ('G', 1), ('H', 7)],
-    'G': [('F', 1), ('I', 3)],
-    'H': [('F', 7), ('I', 2)],
-    'I': [('E', 5), ('G', 3), ('H', 2), ('J', 3)],
-}
+class Puzzle:
+    def __init__(self) -> None:
+        self.visited : List[Node] = []
+        self.expanded : List[Node] = []
+        self.n = 3
+        self.initial = [[] for _ in range(self.n)]
+        self.goal = [[] for _ in range(self.n)]
+    
+    def input(self, variable):
+        # print("Enter state.\n0 represents blank tile")
+        for i in range(self.n):
+            variable[i] = [int(j) for j in input().split(" ")]
+    
+    def solve(self):
+        matrices= []
+        print("Enter initial state:")
+        self.input(self.initial)
+        print("Enter goal state")
+        self.input(self.goal)
+        print("\nSTART")
+        current = Node(self.initial,0,self.goal)
+        matrices.append(current.matrix)
+        self.expanded.append(current)
+        while True:
+            current = self.expanded.pop(0)
+            if current.h_x == 0:
+                break
+            current.printNode()
+            for child in current.generateChildren():
+                if child.matrix not in matrices:
+                    matrices.append(child.matrix)
+                    self.expanded.append(child)
+            self.visited.append(current)
+            self.expanded.sort(key=lambda x : x.f_x,reverse=False)
+        current.printNode()
 
-aStarAlgo('A', 'J')
+
+puzzle = Puzzle()
+puzzle.solve()
